964e97ed528411131d36745c4e5002231aabe843
jairam-2001 <159007536+jairam-2001@users.noreply.github.com>
2026-03-06 15:05:01 +0530

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -66,7 +66,7 @@ export function Navbar() {
             </motion.button>
           ))}
           <motion.a
-          href="/jayaramulu_software_development_engineer_3.4Years.pdf"
+          href="/public/jayaramulu_software_development_engineer_3.4Years.pdf"
           download={true}
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
